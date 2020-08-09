8fac2c6cfc0404f426d53ce04232b24bc7f610fc
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-08-09 09:35:47 +0530

    File added

---
+++ Bit Manipulation/Check Power of 2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if((n & (n-1)) == 0 )
+    {
+        cout<<n <<"is power of 2";
+    }
+    else
+    {
+        cout<<n<<"is not power of 2";
+    }
+}
