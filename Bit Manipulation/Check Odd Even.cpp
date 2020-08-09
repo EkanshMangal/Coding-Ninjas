11d950ebce1d55ecc0873d052af52cbfc0688ec2
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-08-09 09:19:21 +0530

    File added

---
+++ Bit Manipulation/Check Odd Even.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    z=n(1<<0);
+    if (z==0)
+    {
+        cout<<"ODD";
+    }
+    else
+    {
+        cout<<"EVEN";
+    }
+}
